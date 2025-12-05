ea28ca9bf337d263b02b1df4491ec91a51ef1221
Ankita Bhattacharyya <azb6866@psu.edu>
2025-12-05 09:50:29 -0500

    Added comments to the Happiness Wrangling R file

--- Happiness_Wrangling_Data.R
+++ Happiness_Wrangling_Data.R
@@ -1,12 +1,13 @@
-library(tidyverse)
-happiRaw <- read.csv("hapiscore_whr.csv")
-str(happiRaw)
-View(happiRaw)
+library(tidyverse) #loading in tidyverse
+happiRaw <- read.csv("hapiscore_whr.csv") # read in csv file
+str(happiRaw) # checking structure of the data 
+View(happiRaw) # Viewing the data
 
+# cleaning the data
 happiClean <- happiRaw %>%
-  select(-c(geo, X2005:X2022)) %>%
+  select(-c(geo, X2005:X2022)) %>% # removing unneeded columns
   rename(
-    Happiness_Score_2023 = X2023
+    Happiness_Score_2023 = X2023 #rename to a more appropriate name
   )
 
-View(happiClean)
+View(happiClean) # view the clean data
