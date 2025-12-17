bf9a9f8dee2da461d5100a023279288c87611574
Ankita Bhattacharyya <azb6866@psu.edu>
2025-12-16 20:57:22 -0500

    Updated the plot for HDI plot for 2023, and 2022 wrangling code

---
+++ HDI_2022.R
@@ -0,0 +1,20 @@
+library(tidyverse)
+library(rvest)
+
+HDI_2022_raw <- read_html("https://countryeconomy.com/hdi") %>%
+  html_elements(css = "table") %>%
+  html_table()
+view(HDI_2022)
+str(HDI_2022)
+
+HDI_2022 <- (HDI_2022_raw[[1]])
+
+HDI_2022_clean <- HDI_2022 %>%
+  select(-c(,3:5)) %>%
+  separate_wider_delim(
+    cols = Countries,
+    delim = "[",
+    names = c("Country", "junk")
+    ) %>%
+  select(-c(,2)) %>%
+  rename()
--- visualization_happiHDI.R
+++ visualization_happiHDI.R
@@ -9,5 +9,16 @@ HDI_Happiscore_clean <- left_join(
 view(HDI_Happiscore_clean)
 
 ggplot(
-  
+  data = HDI_Happiscore_clean,
+  mapping = aes(
+    x = HDI,
+    y = Happiness_Score_2023
+  )
+) + geom_point() + labs(
+  title = "Human Development Index vs Happiness Score, Year 2023",
+  y = ylab("Happiness Score"),
+  x = xlab("Human Development Index")
+) + geom_smooth(
+  method = "lm"
 )
+  
