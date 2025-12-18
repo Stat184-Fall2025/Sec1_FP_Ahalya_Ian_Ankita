0ed4c867839b2fbf0591caeff96d98dc9b50a48e
Ankita Bhattacharyya <azb6866@psu.edu>
2025-12-17 21:42:14 -0500

    updated comments, QMD file completed

--- HDI_2022.R
+++ HDI_2022.R
@@ -4,8 +4,8 @@ library(rvest)
 HDI_2022_raw <- read_html("https://countryeconomy.com/hdi") %>%
   html_elements(css = "table") %>%
   html_table()
-View(HDI_2022)
-str(HDI_2022)
+#View(HDI_2022)
+#str(HDI_2022)
 
 HDI_2022 <- (HDI_2022_raw[[1]])
 
